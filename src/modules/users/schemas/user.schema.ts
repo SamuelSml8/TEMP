357feb62b8b747769bf47846b0aafa3cb16f7b39
Samuel Sml <veramirandasamuel6@gmail.com>
2024-08-21 22:33:01 -0500

    refactor: const userDocument changed to UserDocument

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type userDocument = User & Document;
+export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
